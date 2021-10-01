ce23ce67d7d650a7d63c420c9ec0b44c86fe15b1
fizz_n_w <rn659296@dal.ca>
2021-10-01 19:22:28 -0300

    Fizz Task 1

--- app/src/main/java/com/example/quick_cash_grp13/MainActivity.java
+++ app/src/main/java/com/example/quick_cash_grp13/MainActivity.java
@@ -3,9 +3,10 @@ package com.example.quick_cash_grp13;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,5 +14,46 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        Test Connection to Firebase
+//        Toast.makeText(MainActivity.this,"Firebase connection success", Toast.LENGTH_LONG).show();
+
+        // get user name from user input
+        EditText loginUserName = (EditText) findViewById(R.id.loginUserName);
+        EditText loginPassword = (EditText) findViewById(R.id.loginPassword);
+
+        // get user name and password to string
+        String userName = (loginUserName.getText().toString());
+        String userPassword = (loginPassword.getText().toString());
+
+        // get login button
+        Button login = (Button) findViewById(R.id.loginCheck);
+        login.setOnClickListener(new View.OnClickListener() {
+            // register system
+            @Override
+            public void onClick(View view) {
+                loginCheck(userName,userPassword);
+            }
+        });
+
+
+        // get register button
+        Button register = (Button) findViewById(R.id.registerCheck);
+        register.setOnClickListener(new View.OnClickListener() {
+            // register system
+            @Override
+            public void onClick(View view) {
+                registerCheck(userName,userPassword);
+
+            }
+        });
+    }
+
+    public void loginCheck(String userName, String userPassword){
+        //TODO
+    }
+
+    public void registerCheck(String userName, String userPassword){
+        //TODO
     }
 }
