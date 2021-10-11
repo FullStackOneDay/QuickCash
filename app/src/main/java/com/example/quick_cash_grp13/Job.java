e5f16dc20d3bf8ca7f4096e7a3353e4c01c9295f
Fizzz <rn659296@dal.ca>
2021-10-11 20:08:59 -0300

    add job and search class

---
+++ app/src/main/java/com/example/quick_cash_grp13/Job.java
@@ -0,0 +1,98 @@
+package com.example.quick_cash_grp13;
+
+import androidx.annotation.NonNull;
+
+public class Job {
+
+    private String jobTitle;
+    private String company;
+
+    // this "filed" is a drop down menu that has several choices.
+    // can reference "https://www.indeed.com/career-advice/finding-a-job/careers-by-field"
+    private String field;
+    private String location;
+    private double salary;
+    private int salary_month;
+
+    public Job(String jobTitle, String company, String field, String location, double salary) {
+        this.jobTitle = jobTitle;
+        this.company = company;
+        this.field = field;
+        this.location = location;
+        this.salary = salary;
+    }
+
+    public Job(String jobTitle, String company, String field, String location) {
+        this.jobTitle = jobTitle;
+        this.company = company;
+        this.field = field;
+        this.location = location;
+    }
+
+    public Job(String jobTitle, String company, String field, String location, int salary_month) {
+        this.jobTitle = jobTitle;
+        this.company = company;
+        this.field = field;
+        this.location = location;
+        this.salary_month = salary_month;
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getField() {
+        return field;
+    }
+
+    public void setField(String field) {
+        this.field = field;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public int getSalary_month() {
+        return salary_month;
+    }
+
+    public void setSalary_month(int salary_month) {
+        this.salary_month = salary_month;
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        return "Job{" +
+                "jobTitle='" + jobTitle + '\'' +
+                ", field='" + field + '\'' +
+                ", location='" + location + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+}
---
+++ app/src/main/java/com/example/quick_cash_grp13/SearchJob.java
@@ -0,0 +1,46 @@
+package com.example.quick_cash_grp13;
+
+import java.util.ArrayList;
+
+public class SearchJob {
+
+    private ArrayList<Job> jobs;
+
+    public SearchJob(ArrayList<Job> jobs) {
+        this.jobs = jobs;
+    }
+
+    public ArrayList<Job> getJobs() {
+        return jobs;
+    }
+
+    public void setJobs(ArrayList<Job> jobs) {
+        this.jobs = jobs;
+    }
+
+    public ArrayList<Job> searchByTitle(String title){
+        // TODO
+        return null;
+    }
+    public  ArrayList<Job> searchByField(String title){
+        // TODO
+        return null;
+
+    }
+    public  ArrayList<Job>searchByCompany(String company){
+        // TODO
+        return null;
+
+    }
+    public  ArrayList<Job> searchByLocation(String title){
+        // TODO
+        return null;
+
+    }
+    public  ArrayList<Job> searchBySalary(String title){
+        // TODO
+        return null;
+
+    }
+
+}
