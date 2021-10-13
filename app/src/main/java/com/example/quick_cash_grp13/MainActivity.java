df1805c7e5ef8b59447f4e8b04220cdd6627efd0
Minh Le <Minh.Le@dal.ca>
2021-10-13 07:44:04 -0300

    + Add new activity as homepage/dashboard + Create an intent to transit to the new activity upon successful login

--- app/src/main/java/com/example/quick_cash_grp13/MainActivity.java
+++ app/src/main/java/com/example/quick_cash_grp13/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.quick_cash_grp13;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -17,8 +18,6 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Hashtable;
 
 
@@ -76,15 +75,19 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
                 if (task.isSuccessful()) {
+                    Toast.makeText(MainActivity.this,"Login succeed",Toast.LENGTH_SHORT).show();
                     // Sign in success, update UI with the signed-in user's information
                     FirebaseUser user = mAuth.getCurrentUser();
                     //update UI and move to next activity
+                    //Start next activity
+                    Intent home = new Intent(getApplicationContext(),HomeActivity.class);
+                    startActivity(home);
+
                 } else {
                     // If sign in fails, display a message to the user.
                     Toast.makeText(MainActivity.this, "Authentication failed.",
                             Toast.LENGTH_SHORT).show();
                     //update UI and move to next activity
-                    FirebaseUser user = null;
                 }
             }
         });
