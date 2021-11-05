8ca8f84424da6214b6109c64486513bbdfca9aea
Yaman Odah <ym716479@dal.ca>
2021-11-05 16:39:50 -0300

    fixed error in junit class

--- app/src/test/java/com/example/quick_cash_grp13/ExampleUnitTest.java
+++ app/src/test/java/com/example/quick_cash_grp13/ExampleUnitTest.java
@@ -1,10 +1,14 @@
 package com.example.quick_cash_grp13;
 
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule;
+
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -12,54 +16,57 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+    MainActivity mainActivity;
+
+    @Rule
+    public InstantTaskExecutorRule instantTaskExecutorRule = new InstantTaskExecutorRule();
+
+    @Before
+    public void setup() {
+        mainActivity = new MainActivity();
+    }
+
+    @After
+    public void tearDown(){
+        System.gc();
+    }
 
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
     }
 
-    /*static MainActivity mainActivity;
 
-    @BeforeClass
-    public static void setup() {
-        mainActivity = new MainActivity();
-    }
-
-    @AfterClass
-    public static void tearDown() {
-        System.gc();
-    }
-
-    *//**
+    /**
      * check for empty email
-     *//*
+     */
     @Test
     public void checkEmailIsEmpty() {
         assertTrue(mainActivity.isEmailEmpty(""));
     }
 
-    *//**
+    /**
      * check for nonempty email
-     *//*
+     */
     @Test
     public void checkEmailIsNotEmpty() {
         assertFalse(mainActivity.isEmailEmpty("qwerty123@dal.ca"));
     }
 
-    *//**
+    /**
      * check for valid email
-     *//*
+     */
     @Test
     public void checkEmailIsValid() {
         assertTrue(mainActivity.isEmailValid("qwerty123@dal.ca"));
     }
 
-    *//**
+    /**
      * check for invalid email
-     *//*
+     */
     @Test
     public void checkEmailIsInvalid() {
         assertFalse(mainActivity.isEmailValid("abc123gmail.com"));
     }
-*/
+
 }
