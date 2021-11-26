2ba3a93cb9ac763a0705fefcc875d2f1a9a83b42
Your Name <you@example.com>
2021-11-26 01:55:54 -0400

    Basic integration of Paypal to app, needs work

---
+++ app/src/main/java/com/example/quick_cash_grp13/PaypalConfig.java
@@ -0,0 +1,11 @@
+package com.example.quick_cash_grp13;
+
+public class PaypalConfig {
+    /*
+    Paypal account login, in case changes need to be made.
+        email: er392011@dal.ca
+        pw: 3130Quickcashapp
+    */
+
+    public static final String PAYPAL_CLIENT_ID = "AQgKzgwxAcPf-YVpHrLnBL8PC1csnP41F8h7vgG83hrw35aXY9_FUWxbpKZwsUzP08kQ-czwSipYLZ1N";
+}
